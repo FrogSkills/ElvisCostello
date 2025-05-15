a24ecb4bf19a613d7244be00b20ca9227b23ad06
Miguel <mgamez20@icloud.com>
2025-05-14 21:21:48 -0400

    Done.

--- ElvisCostello/ContentView.swift
+++ ElvisCostello/ContentView.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var movingImage = ""
+    @State private var movingText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .foregroundStyle(.purple)
+                .fontWeight(.black)
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            Image(systemName: movingImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            
+            
+            Text("\(movingText)")
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+            
+            Spacer()
+
+            HStack {
+                Button("Peace") {
+                    movingImage = "peacesign"
+                    movingText = "Peace"
+                }
+                
+                Button("Love") {
+                    movingImage = "heart"
+                    movingText = "Love"
+                }
+                
+                Button("Understanding") {
+                    movingImage = "lightbulb"
+                    movingText = "Understanding"
+                }
+            }
+            .tint(.purple)
+            .buttonStyle(.bordered)
+//            .buttonStyle(.borderedProminent)
+//            .foregroundStyle(.white)
+            
+            
         }
         .padding()
     }
