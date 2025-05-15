5df09de538144abde8b7a1ba95bc34a34f0258a1
Miguel <mgamez20@icloud.com>
2025-05-14 20:45:26 -0400

    Initial Commit

---
+++ ElvisCostello/ElvisCostelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloApp.swift
+//  ElvisCostello
+//
+//  Created by Miguel on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElvisCostelloTests/ElvisCostelloTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloTests.swift
+//  ElvisCostelloTests
+//
+//  Created by Miguel on 5/14/25.
+//
+
+import Testing
+@testable import ElvisCostello
+
+struct ElvisCostelloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
